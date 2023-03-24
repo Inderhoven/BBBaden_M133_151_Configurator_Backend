e526d9bb54a28fd2921683990349b74e9fbf1427
Patrik Gisler <patrik.gisler@gmx.net>
2023-03-24 09:08:16 +0100

    added characterSettingsSerializer

--- backend/authorizationServer/views.py
+++ backend/authorizationServer/views.py
@@ -1,31 +1,54 @@
 from rest_framework.views import APIView
 from django.conf import settings
-from rest_framework import authentication, exceptions
+from rest_framework import authentication, exceptions, serializers
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from django.http import JsonResponse
+from .models import CharacterSettings
 
 import jwt
 
 
 # Create your views here.
-class CharacterView(APIView):
-    def post(self, request):
-        pass
+class CharacterSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CharacterSettings
+        fields = '__all__'
 
-    def get(self, request):
+class CharacterView(APIView):
+
+    def _authenticate_user(self, request):
         auth_header = authentication.get_authorization_header(request)
         if not auth_header:
-            return None
+            raise exceptions.AuthenticationFailed('Authorization header is missing')
         
         try:
             token = auth_header.decode('utf-8')
             payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=['HS256'])
-
-        except:
-            raise exceptions.AuthenticationFailed('Invalid authentication token')
-        
-        try:
             user = User.objects.get(id=payload['userId'])
-        except:
+        except jwt.exceptions.DecodeError:
+            raise exceptions.AuthenticationFailed('Invalid authentication token')
+        except User.DoesNotExist:
             raise exceptions.AuthenticationFailed('User not found')
         
-        pass
+        return user
+
+    def post(self, request):
+        user = self._authenticate_user(request)
+
+        if user is not None:
+            pass
+
+
+        return Response({'message': 'Not authenticated!'}, status=401)
+
+    def get(self, request):
+        user = self._authenticate_user(request)
+
+        if user is not None:
+            character_settings = CharacterSettings.objects.get(user=user)
+            serializer = CharacterSettingsSerializer(character_settings)
+            return Response(serializer.data)
+        
+        return Response({'message': 'Not authenticated!'}, status=401)
+        
